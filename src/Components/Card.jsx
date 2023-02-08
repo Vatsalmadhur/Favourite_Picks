5c27550a54b77556043cf2349a58a5c193340c37
Madhur <bhartimadhur21@gmail.com>
2023-02-08 21:20:26 +0530

    First commit

---
+++ src/Components/Card.jsx
@@ -0,0 +1,9 @@
+<div className="outerCard">
+  <p className="info1"></p>
+  <p className="info2"></p>
+  <div className="btn">
+    <a href="" className="link">
+      <p className="watch"></p>
+    </a>
+  </div>
+</div>;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import './Navbar.css';
+function Navbar(){
+    return(
+<>
+<div className="box">
+    <p className="navtxt">Top Netflix Series</p>
+</div>
+</>
+    )
+}
+export default Navbar;

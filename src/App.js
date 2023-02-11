c8e03a51ea89adc3cf1242fa9a3d0e2f8f10ae12
Madhur <bhartimadhur21@gmail.com>
2023-02-12 02:00:06 +0530

    Optimised

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Card from "./Components/Card";
-import DK from "./images/DarkWS.jpeg"
-import ST from "./images/StrangerThingsWS.jpeg"
-import SG from "./images/SquidgameWS.jpg"
-import MH from "./images/MoneyheistWS.jpeg"
-import WD from "./images/WednesdayWS.jpeg"
-import HH from "./images/TheHauntingWS.jpg"
-
-
-
-
+import SData from "./SData";
 
 function App() {
   return (
@@ -18,46 +9,17 @@ function App() {
       <Navbar />
 
       <div className="mainBox">
-        <Card
-          sname="Stranger Things"
-          imgsrc={ST}
-          title="A Netflix Orignals"
-          link="https://www.netflix.com/in/title/80057281"
-        />
-        <Card
-          sname="Dark"
-          imgsrc={DK}
-          title="A Netflix Orignals"
-          link="https://www.netflix.com/in/title/80100172"
-        />
-
-        <Card
-          sname="Squid Game"
-          imgsrc={SG}
-          title="A Netflix Orignals"
-          link="https://www.netflix.com/in/title/81040344"
-        />
-
-        <Card
-          sname="Money Heist"
-          imgsrc={MH}
-          title="A Netflix Orignals"
-          link="https://www.netflix.com/in/title/80192098"
-        />
-
-        <Card
-          sname="Wednesday"
-          imgsrc={WD}
-          title="A Netflix Orignals"
-          link="https://www.netflix.com/in/title/81231974"
-        />
-
-        <Card
-          sname="The Haunting of Hill House"
-          imgsrc={HH}
-          title="A Netflix Orignals"
-          link="https://www.netflix.com/in/title/80189221"
-        />
+        {SData.map(function ncard(val,index) {
+          return (
+            <Card
+              sname={val.sname}
+              imgsrc={val.imgsrc}
+              title={val.title}
+              link={val.link}
+              key={index}
+            />
+          );
+        })}
       </div>
     </>
   );
--- src/SData.jsx
+++ src/SData.jsx
@@ -1,38 +1,46 @@
+import DK from "./images/DarkWS.jpeg"
+import ST from "./images/StrangerThingsWS.jpeg"
+import SG from "./images/SquidgameWS.jpg"
+import MH from "./images/MoneyheistWS.jpeg"
+import WD from "./images/WednesdayWS.jpeg"
+import HH from "./images/TheHauntingWS.jpg"
+
 const SData = [
   {
     sname: "Dark",
-    imgsrc: "../public/images/",
+    imgsrc: DK,
     title: "A Netflix Orignals",
     link: "https://www.netflix.com/in/title/80100172",
   },
   {
     sname: "Stranger Things",
-    imgsrc: "",
+    imgsrc: ST,
     title: "A Netflix Orignals",
     link: "https://www.netflix.com/in/title/80057281",
   },
   {
     sname: "Squid Game",
-    imgsrc: "./images/DarkWS.jpeg",
+    imgsrc: SG,
     title: "A Netflix Orignals",
     link: "https://www.netflix.com/in/title/81040344",
   },
   {
     sname: "Money Heist",
-    imgsrc: "",
+    imgsrc: MH,
     title: "A Netflix Orignals",
     link: "https://www.netflix.com/in/title/80192098",
   },
   {
     sname: "Wednesday",
-    imgsrc: "",
+    imgsrc: WD,
     title: "A Netflix Orignals",
     link: "https://www.netflix.com/in/title/81231974",
   },
   {
     sname: "The Haunting of Hill House",
-    imgsrc: "",
+    imgsrc: HH,
     title: "A Netflix Orignals",
     link: "https://www.netflix.com/in/title/80189221",
   },
 ];
+export default SData;

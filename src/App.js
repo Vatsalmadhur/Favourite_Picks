e6ff1ab0e9c39f49c542be03a729e5c4d6543ce9
Madhur <bhartimadhur21@gmail.com>
2023-02-11 23:45:25 +0530

    Added images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Card from "./Components/Card";
+import DK from "./images/DarkWS.jpeg"
+import ST from "./images/StrangerThingsWS.jpeg"
+import SG from "./images/SquidgameWS.jpg"
+import MH from "./images/MoneyheistWS.jpeg"
+import WD from "./images/WednesdayWS.jpeg"
+import HH from "./images/TheHauntingWS.jpg"
+
+
 
 
 
@@ -12,41 +20,41 @@ function App() {
       <div className="mainBox">
         <Card
           sname="Stranger Things"
-          imgsrc=""
+          imgsrc={ST}
           title="A Netflix Orignals"
           link="https://www.netflix.com/in/title/80057281"
         />
         <Card
           sname="Dark"
-          imgsrc="./images/Dark.jpeg"
+          imgsrc={DK}
           title="A Netflix Orignals"
           link="https://www.netflix.com/in/title/80100172"
         />
 
         <Card
           sname="Squid Game"
-          imgsrc=""
+          imgsrc={SG}
           title="A Netflix Orignals"
           link="https://www.netflix.com/in/title/81040344"
         />
 
         <Card
           sname="Money Heist"
-          imgsrc=""
+          imgsrc={MH}
           title="A Netflix Orignals"
           link="https://www.netflix.com/in/title/80192098"
         />
 
         <Card
           sname="Wednesday"
-          imgsrc=""
+          imgsrc={WD}
           title="A Netflix Orignals"
           link="https://www.netflix.com/in/title/81231974"
         />
 
         <Card
           sname="The Haunting of Hill House"
-          imgsrc=""
+          imgsrc={HH}
           title="A Netflix Orignals"
           link="https://www.netflix.com/in/title/80189221"
         />
